b759f36af0003e014144abe2ec7570517e72ffbd
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-11-04 15:02:23 -0300

    chore: relationship mapping

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -8,7 +8,11 @@ interface answerQuestionUseCaseRequest {
 
 export class answerQuestionUseCase {
   execute({instructorId, questionId, content}: answerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
+    const answer = new Answer({
+      content, 
+      authorId: instructorId, 
+      questionId
+    })
 
     return answer
   }
