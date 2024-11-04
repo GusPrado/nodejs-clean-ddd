5b39be42791e8d0c178aec362df5067dff794551
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-11-04 18:39:39 -0300

    chore: Path aliases & Vitest globals

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import {expect, test} from 'vitest'
 import { answerQuestionUseCase } from './answer-question'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { Answer } from '../entities/answer'
