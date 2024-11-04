b5e150e4f9b42994f7dd29497672b97d78be52a1
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-11-04 17:17:18 -0300

    refactor: Abstract entity creation

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,6 +1,7 @@
 import { Slug } from "./value-objetcs/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
   authodId: UniqueEntityID
@@ -11,4 +12,17 @@ interface QuestionProps {
   createdAt: Date
   updatedAt?: Date
 }
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>, 
+    id?: UniqueEntityID
+  ) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return question
+  }
+}
+
