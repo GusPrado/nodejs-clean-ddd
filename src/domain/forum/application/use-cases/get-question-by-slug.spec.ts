3bfb47c23bdea40ab89875b44d4f44b468b8d384
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-11-09 17:40:28 -0300

    feat: Add find by slug use case - config test path - fix eslint issues on vite.config

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Question } from '../../enterprise/entities/question'
+import { Slug } from '../../enterprise/entities/value-objetcs/slug'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get a Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('Should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityID(),
+      title: 'Example question',
+      slug: Slug.create('example-question'),
+      content: 'Here is my question.',
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(newQuestion.title)
+    expect(question.slug.value).toEqual('example-question')
+  })
+})
