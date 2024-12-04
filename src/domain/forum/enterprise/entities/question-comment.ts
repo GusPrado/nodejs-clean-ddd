71e1d0728cebfc4e61a76f45cc67b605b984d198
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-12-03 21:04:46 -0300

    feat: Add comment on question use case

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -2,19 +2,19 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { CommentProps, Comment } from './comment'
 
-export interface QuestioncommentProps extends CommentProps {
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
 }
-export class Questioncomment extends Comment<QuestioncommentProps> {
+export class QuestionComment extends Comment<QuestionCommentProps> {
   get questionId() {
     return this.props.questionId
   }
 
   static create(
-    props: Optional<QuestioncommentProps, 'createdAt'>,
+    props: Optional<QuestionCommentProps, 'createdAt'>,
     id?: UniqueEntityID,
   ) {
-    const question = new Questioncomment(
+    const question = new QuestionComment(
       {
         ...props,
         createdAt: props.createdAt ?? new Date(),
