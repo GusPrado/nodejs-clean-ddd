71c932812ae5468b5341181150a4d72d12b5c7b8
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-12-03 22:19:38 -0300

    feat: Add more use cases - Delete comment on question & answer - List comments on question & answer

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -0,0 +1,33 @@
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+interface FetchAnswerCommentsUseCaseRequest {
+  answerId: string
+  page: number
+}
+
+interface FetchAnswerCommentsUseCaseResponse {
+  answerComments: AnswerComment[]
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    page,
+    answerId,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments =
+      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+        page,
+      })
+
+    if (!answerComments) {
+      throw new Error('Answer without comments.')
+    }
+
+    return {
+      answerComments,
+    }
+  }
+}
